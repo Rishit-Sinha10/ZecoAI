9bc0647bd2c27bd4a93856cf763e6576e661381c
Ramesh1234-ai <Sinharishit04@gmail.com>
2026-07-16 15:13:08 +0530

    Adding Vercel Analytics and Google Search console

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { ClerkProvider } from '@clerk/clerk-react'
 import './index.css'
 import App from './App'
-
+import { Analytics } from "@vercel/analytics/next";
 const CLERK_PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!CLERK_PUBLISHABLE_KEY) {
@@ -13,6 +13,7 @@ if (!CLERK_PUBLISHABLE_KEY) {
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY}>
+      <Analytics/>
       <App/>
     </ClerkProvider>
   </BrowserRouter>,
